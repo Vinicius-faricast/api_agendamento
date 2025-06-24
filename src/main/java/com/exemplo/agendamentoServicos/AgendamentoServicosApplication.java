998d39b04adbfefb72e824425b30a54d1a6f280e
Vinicius <vfarias.designer@gmail.com>
2025-06-23 21:01:06 -0300

    initial project structure

---
+++ src/main/java/com/exemplo/agendamentoServicos/AgendamentoServicosApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo.agendamentoServicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoServicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoServicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exemplo/agendamentoServicos/AgendamentoServicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemplo.agendamentoServicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoServicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
